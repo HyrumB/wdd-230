15e542cee4c8ef6d87697c26e57d293ad26f1092
HyrumB <hyrumbullock@gmail.com>
2024-04-06 15:45:17 -0600

    finished join pg

--- chamber/scripts/form.js
+++ chamber/scripts/form.js
@@ -2,5 +2,12 @@ const form = document.querySelector("form");
 
   form.addEventListener("submit", function(event) {
     event.preventDefault(); // Prevent default form submission
-    window.location.href = "landing.html";
+    window.location.href = "thankyou.html";
   });
+
+
+// Get the hidden input field
+const formLoadTimeInput = document.getElementById("form_load_time");
+const timestamp = new Date().getTime(); // Returns milliseconds since Unix epoch
+// Set the value of the hidden input field
+formLoadTimeInput.value = timestamp;
