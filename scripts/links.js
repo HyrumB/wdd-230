a73a397381e201741c70d74e0dd4e879ad77598e
HyrumB <hyrumbullock@gmail.com>
2024-04-06 23:25:22 -0600

    fixed links.js

--- scripts/links.js
+++ scripts/links.js
@@ -5,32 +5,33 @@ const linksURL = "https://hyrumb.github.io/wdd230/data/links.json";
 async function getLinks() {
   const response = await fetch(linksURL);
   const data = await response.json();
-  // console.log(data);
+  console.log(data);
   displayLinks(data);
 }
 
 function displayLinks(data) {
-  const weekList = document.querySelector('#weekList');
-  for (let i = 0; i < weeks.week.length; i++) {
-      
-      const week = document.createElement('li');
-      week.textContent = `${weeks.weeks[i].week} :`;
-      weekList.appendChild(week);
-      for (let j = 0; j < weeks.weeks[i].links.length; j++) {
+  const activityList = document.querySelector('#activities');
 
+  for (let i = 0; i < data.weeks.length; i++) {
+      const week = document.createElement('li');
+
+      week.textContent = `${data.weeks[i].week} :`;
+      activityList.appendChild(week);
+
+      for (let j = 0; j < data.weeks[i].links.length; j++) {
           const link = document.createElement('a');
-          link.href = `${weeks.weeks[i].links[j].url}`;
-          link.textContent = `${weeks.weeks[i].links[j].title} |`;
-          if (j === weeks.weeks[i].links.length - 1) {
-              link.textContent = `${weeks.weeks[i].links[j].title}`;
+
+          link.href = `${data.weeks[i].links[j].url}`;
+          link.textContent = `${data.weeks[i].links[j].title} |`;
+          
+          if (j === data.weeks[i].links.length - 1) {
+              link.textContent = `${data.weeks[i].links[j].title}`;
           }
           week.appendChild(link);
 
 
       }
   }
-  console.log(weekList);
-
 }
 
 
